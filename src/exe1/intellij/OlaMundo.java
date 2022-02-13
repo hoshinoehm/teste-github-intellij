e78d0757834cb569aca862767796213629089f59
hoshinoehm <hoshinoehm@gmail.com>
2022-02-13 18:46:14 -0300

    frist commit

---
+++ src/exe1/intellij/OlaMundo.java
@@ -0,0 +1,16 @@
+package exe1.intellij;
+
+public class OlaMundo {
+
+    public static void main(String[] args) {
+
+        int a = 4 ;
+        int b = 3 ;
+
+        int soma = a + b ;
+        int sub  = a - b ;
+
+        System.out.println("O valor da soma é: "+soma);
+        System.out.println("O valor da subtração é: "+sub);
+    }
+}
